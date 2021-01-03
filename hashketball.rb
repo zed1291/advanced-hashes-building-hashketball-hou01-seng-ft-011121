16c3dcdb338d8be8c6a349f6a4ca1d612917acc7
zed <zed@kolk.me>
2021-01-03 19:29:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,63 +5,62 @@ def game_hash
       :colors => ["Black", "White"],
 
       :players => [
-        :player_name => {
-          :alan_anderson => {
-            :player_name => "Alan Anderson",
-            :number => 0,
-            :shoe => 16,
-            :points => 22,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 3,
-            :blocks => 1,
-            :slam_dunks => 1
-          },
-           :reggie_evans => {
-            :player_name => "Reggie Evans",
-            :number => 30,
-            :shoe => 14,
-            :points => 12,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 12,
-            :blocks => 1,
-            :slam_dunks => 7,
-          },
-           :brook_lopez => {
-            :player_name => "Brook Lopez",
-            :number => 11,
-            :shoe => 17,
-            :points => 17,
-            :rebounds => 19,
-            :assists => 10,
-            :steals => 3,
-            :blocks => 1,
-            :slam_dunks => 15,
-          },
-          :mason_plumlee => {
-            :player_name => "Mason Plumlee",
-            :number => 1,
-            :shoe => 19,
-            :points => 26,
-            :rebounds => 11,
-            :assists => 6,
-            :steals => 3,
-            :blocks => 8,
-            :slam_dunks => 5,
-          },
-          :jason_terry => {
-            :player_name => "Jason Terry",
-            :number => 31,
-            :shoe => 15,
-            :points => 19,
-            :rebounds => 2,
-            :assists => 2,
-            :steals => 4,
-            :blocks => 11,
-            :slam_dunks => 1,
+
+          :player_name => {
+            "Alan Annderson" => [
+              :number => 0,
+              :shoe => 16,
+              :points => 22,
+              :rebounds => 12,
+              :assists => 12,
+              :steals => 3,
+              :blocks => 1,
+              :slam_dunks => 1],
+
+            "Reggie Evans" => [
+              :number => 30,
+              :shoe => 14,
+              :points => 12,
+              :rebounds => 12,
+              :assists => 12,
+              :steals => 12,
+              :blocks => 1,
+              :slam_dunks => 7,
+            ],
+
+            "Brook Lopez"=> [
+              :number => 11,
+              :shoe => 17,
+              :points => 17,
+              :rebounds => 19,
+              :assists => 10,
+              :steals => 3,
+              :blocks => 1,
+              :slam_dunks => 15,
+            ],
+
+            "Mason Plumlee"=> [
+              :number => 1,
+              :shoe => 19,
+              :points => 26,
+              :rebounds => 11,
+              :assists => 6,
+              :steals => 3,
+              :blocks => 8,
+              :slam_dunks => 5,
+            ],
+
+            "Jason Terry"=> [
+              :number => 31,
+              :shoe => 15,
+              :points => 19,
+              :rebounds => 2,
+              :assists => 2,
+              :steals => 4,
+              :blocks => 11,
+              :slam_dunks => 1,
+            ],
           }
-        }
       ]
     },
     :away => {
