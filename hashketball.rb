eb033cf6a7d9d262dcc2302c0fb9cb9ba68981e9
zed <zed@kolk.me>
2021-01-03 20:29:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,12 @@
 def game_hash
-  hasketball = {
+  hashketball = {
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
+
+        #   it 'returns the correct player data for the :home team' do
+        # home_players = [alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry]
+        # names = game_hash[:home][:players].map {|player| player[:player_name]}
 
       :players => [
         {:player_name => "Alan Anderson",
@@ -37,7 +41,7 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 15,
         },
-
+            
         {:player_name => "Mason Plumlee",
         :number => 1,
         :shoe => 19,
@@ -65,6 +69,61 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
+
+      {:player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+        },
+
+        {:player_name => "Bismack Biyombo",
+        :number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 142,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10,
+        },
+
+        {:player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5,
+        },
+            
+        {:player_name => "Ben Gordon",
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0,
+        },
+
+        {:player_name => "Kemba Walker",
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5,
+        :slam_dunks => 12,
+        }
       ]
     }
   }
